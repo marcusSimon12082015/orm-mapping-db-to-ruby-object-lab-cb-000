d5d9df285854c17f36f51aa7136f358bd1c6d43e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-07 19:03:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,6 +83,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
+    binding.pry
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
