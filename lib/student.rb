542c552ef48eefa0c8fadd7cc57a9e5536b696a1
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-07 18:12:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,7 +56,7 @@ class Student
     DB[:conn].execute(sql)
   end
   def self.count_all_students_in_grade_9
-    sql <<-SQL
+    sql = <<-SQL
       SELECT * FROM students WHERE grade = '9th'
     SQL
     DB[:conn].execute(sql).map do |row|
